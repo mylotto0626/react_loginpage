f5a97867381d01b72a5241656c784938d03786f1
Hannah Kim <65204055+mylotto0626@users.noreply.github.com>
2023-01-25 16:04:48 +0900

    bcrypt로 임호화 구현

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express"); //express 불러오기
 const app = express(); // express 사용
 const port = 3000;
 const bodyParser = require("body-parser");
-const config = require('./config/key');
+const config = require("./config/key");
 //스키마 가져오기
 const { User } = require("./models/user");
 
@@ -25,6 +25,29 @@ app.post("/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (res, req) => {
+  //요청된 아이디를 데이터베이스에서 있는지 찾는다
+  User.findOne({ id: req.body.id }, (err, user) => {
+    if (!userInfo) {
+      return res.json({
+        loginSuccess: false,
+        message: "id가 맞지 않습니다",
+      });
+    }
+    //요청된 아이디가 데이터베이스에 있다면 비밀번호가 맞는지 확인
+    userInfo.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 맞지 않습니다 ",
+        })
+        user.getToken((err,user)=>{
+            
+        })
     });
   });
 });
